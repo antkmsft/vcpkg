3e0ea2f59aff59e1976867d6e433db42cf64d05a
Anton Kolesnyk <antkmsft@users.noreply.github.com>
2021-04-16 15:38:48 -0700

    identity 1.1 (same-major)

--- ports/azure-identity-cpp/portfile.cmake
+++ ports/azure-identity-cpp/portfile.cmake
@@ -1,8 +1,8 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO antkmsft/azure-sdk-for-cpp
-    REF core-1.0_identity-1.0_same-major
-    SHA512 40fe4cef6ded61bf36a8f41edf6c160065347cd45b209b46d12f8a57e4469cdfa4b2ca9f698e3f0c7d0ae091dd0b23dce368a953da42275c231053788da16802
+    REF core-1.1_identity-1.1_same-major
+    SHA512 184f3ec802e3663b8a17d7893309b84e9de7c071509bc5f0a970e6b7562e510a22137e82aed20d6077d1dcae169218559f83539bbb6f4451c7ea61879a20a063
 )
 
 vcpkg_configure_cmake(
