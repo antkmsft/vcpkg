c88d26a8a24636201982cb6beaf20473ac437f7e
Anton Kolesnyk <antkmsft@users.noreply.github.com>
2023-09-19 13:32:12 -0700

    [azure-messaging-eventhubs-cpp] Shorten source paths

--- ports/azure-messaging-eventhubs-cpp/portfile.cmake
+++ ports/azure-messaging-eventhubs-cpp/portfile.cmake
@@ -3,6 +3,8 @@ vcpkg_from_github(
     REPO Azure/azure-sdk-for-cpp
     REF azure-messaging-eventhubs_1.0.0-beta.2
     SHA512 3677edf50218c39e378a968661c0ec0600b8760d3af33f204f3267e104449c0aef42b9520cb63b5e5810b89bf91f06979d009ffdd6d6d04d2c14e1a21f0e111f
+    PATCHES
+        00100-shorten_paths.patch
 )
 
 vcpkg_cmake_configure(
